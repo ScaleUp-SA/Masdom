99f80ad584f4ed1022d444948b0b5df1fde3a3f9
Metwally sayed <metwallysayed1999@gmail.com>
2023-11-15 19:47:35 +0200

    removing old middleware

--- src/middleware.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-import { NextRequest } from "next/server";
-
-export { default } from "next-auth/middleware";
-
-export const config = {
-  matcher: ["/profile", "/profile/:path*"],
-};
