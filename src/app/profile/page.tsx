0dd7b2915ee3e92cbfc6d8d0774c3bad17855cc1
islam <eslamkhalil851@gmail.com>
2023-12-14 19:36:47 +0200

    create add car page and part of profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 type Props = {};
@@ -7,7 +8,25 @@ export const metadata = {
 };
 
 const Page = (props: Props) => {
-  return <div className="h-screen">Profile</div>;
+  return (
+    <div className="h-screen grid grid-cols-6">
+      <div className="bg-slate-800 col-span-1 p-4 text-white text-base">
+        <span>
+          {/* add image of user */}
+          <h4 className="text-3xl">username</h4>
+        </span>
+        <ul className="mt-6">
+          <li className="mt-2 text-white hover:text-green-400"><Link href={"profile/listcar"}>أضف سيارة جديدة</Link></li>
+          <li className="mt-2 text-white hover:text-green-400">link</li>
+          <li className="mt-2 text-white hover:text-green-400">link</li>
+          <li className="mt-2 text-white hover:text-green-400">link</li>
+        </ul>
+      </div>
+      <div className="col-span-5 p-4">
+        Profile content
+      </div>
+    </div>
+  );
 };
 
 export default Page;
