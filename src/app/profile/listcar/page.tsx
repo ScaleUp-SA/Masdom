e7c9927f30c1473dff7a27fbe03aeae8d4cc0e51
islam <eslamkhalil851@gmail.com>
2023-12-23 17:37:48 +0200

    upload listcar page

--- src/app/profile/listcar/page.tsx
+++ src/app/profile/listcar/page.tsx
@@ -72,7 +72,7 @@ const page = (props: Props) => {
             <span>
               <label className="flex flex-col gap-2 w-full">
                 ناقل الحركة
-                <select 
+                <select
                   required
                   className="p-2 rounded border"
                   value={formData.transmission}
