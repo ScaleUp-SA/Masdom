191de9079806d7e2e79b037ea06b1bc0ca3eaf96
Metwally sayed <metwallysayed1999@gmail.com>
2024-01-11 20:24:55 +0200

    fixing schema error messages

--- src/validationSchemas/loginValidation.ts
+++ src/validationSchemas/loginValidation.ts
@@ -4,11 +4,15 @@ export const loginFormSchema = z.object({
   email: z
     .string({
       invalid_type_error: "يجب ادخال احرف",
+      required_error: "يجب ادخال البريد الالكتروني",
     })
     .email("ادخل بريد الكتروني صالح")
     .max(255, "لقد تخطيت العدد المسموح"),
   password: z
-    .string({ invalid_type_error: "يجب ادخال احرف و ارقام و رموز" })
+    .string({
+      invalid_type_error: "يجب ادخال احرف و ارقام و رموز",
+      required_error: "يجب ادخال كلمة السر",
+    })
     .min(8, "يجب علي الاقل ادخال 8 احرف و ارقام")
     .max(18, "لقد تخطيت العدد المسموح"),
 });
--- src/validationSchemas/signupValidation.ts
+++ src/validationSchemas/signupValidation.ts
@@ -17,12 +17,16 @@ export const signupSchema: ZodSchema<{
     .email("ادخل بريد الكتروني صالح")
     .max(225, "لقد تخطيت العدد المسموح"),
   password: z
-    .string({ invalid_type_error: "يجب ادخال احرف و ارقام و رموز" })
+    .string({
+      invalid_type_error: "يجب ادخال احرف و ارقام و رموز",
+      required_error: "يجب ادخال كلمة السر",
+    })
     .min(8, "يجب علي الاقل ادخال 8 احرف و ارقام")
     .max(18, "لقد تخطيت العدد المسموح"),
   username: z
     .string({
       invalid_type_error: "يجب ادخال احرف",
+      required_error: "يجب ادخال الاسم",
     })
     .min(2, "يجب علي الاقل ادخال 2 احرف و ارقام")
     .max(10, "لقد تخطيت العدد المسموح"),
